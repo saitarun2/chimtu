0eb00b68dad9dc8b01fdcb026291c5a9feb72bf6
Tarun <tarun.nagasai.92@gmail.com>
2023-06-20 16:58:52 +0530

    '2nd touch'

--- script.js
+++ script.js
@@ -4,37 +4,43 @@ const ul = document.getElementById('tasks');
 const box = document.getElementById('checkbox');
 
 function addtask() {
-  if (input.value.length === 0) {
+  if (input.value.trim().length === 0) {
     alert('type something');
   } else {
     let task = input.value;
     let li = document.createElement('li');
     let checkbox = document.createElement('input');
     let label = document.createElement('label');
+    let t1 = document.createElement('input');
+
+    t1.type = 'text';
     checkbox.type = 'checkbox';
     label.for = task;
-    label.innerText = task;
     li.appendChild(checkbox);
     checkbox.id = 'checkbox';
     li.appendChild(label);
+    t1.id = 't1'
+    label.appendChild(t1)
+    t1.value = task;
     ul.appendChild(li);
-    if (checkbox.checked) {
-      label.innerHTML.value = '<s>' + value + '</s>'
-    }
-    console.log
     input.value = '';
     savedata();
   }
 }
 
 function deletetask() {
-  const checkboxes = document.querySelectorAll('#tasks input[type="checkbox"]');
-  checkboxes.forEach(function (checkbox) {
-    if (checkbox.checked) {
-      const li = checkbox.parentNode;
+  const checkbox = document.querySelectorAll('#tasks input[type="checkbox"]');
+  let checkedtasks = false;
+  checkbox.forEach(function(checkbox){
+    if (checkbox.checked){
+      checkedtasks = true;
+      const li= checkbox.parentNode;
       ul.removeChild(li);
     }
   });
+  if(!checkedtasks){
+    alert('Select to delete')
+  }
   savedata();
 }
 
